c1b5c768480d955e6db4b156067ffc3f935bdf20
Abdullaah Yusuf <abdy88@gmail.com>
2022-09-29 13:35:12 +0530

    adding elements in different order

---
+++ LinkedListMenu.java
@@ -0,0 +1,27 @@
+package bl.com.linkedlistds;
+
+public class LinkedListMenu {
+
+	public static void main(String[] args) {
+		LinkedListOperations list = new LinkedListOperations();
+
+		list.insertFirst(70);
+		list.insertFirst(30);
+		list.insertFirst(56);
+		list.display();
+	}
+
+}
+
+class Node {
+
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+
+	}
+
+}
